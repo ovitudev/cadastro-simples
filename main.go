a53c1e4afab8417ba53d7172a1f1de8450d7ecba
ovitudev <maximianodev@gmail.com>
2024-07-29 22:17:29 -0300

    Add router for delete and search

--- main.go
+++ main.go
@@ -15,8 +15,10 @@ func main() {
 	router := mux.NewRouter()
 	log.Println("Executando ...")
 
-	router.HandleFunc("/cadastro", controller.SignUp).Methods("POST") // Rota para cadastros
-	router.HandleFunc("/lista", controller.GetTokens).Methods("GET") // Rota para ver cadastros
+	router.HandleFunc("/cadastro", controller.SignUp).Methods("POST")      // Rota para cadastros
+	router.HandleFunc("/limpar", controller.GetClean).Methods("DELETE")    // Rota para limpar TODOS os cadastros
+	router.HandleFunc("/lista", controller.GetTokens).Methods("GET")       // Rota para ver cadastros
+	router.HandleFunc("/user/{id}", controller.GetUserByID).Methods("GET") // Rota para usuário através do ID
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
